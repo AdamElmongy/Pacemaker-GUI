036cd70858a8bb7fa264fec55a5a6905ef761369
abushr2 <abushr2@mcmaster.ca>
2023-10-10 21:15:01 -0400

    Test after main merge error
    fixed MainMenu.py and added myself as a user

--- MainMenu.py
+++ MainMenu.py
@@ -5,30 +5,18 @@ def main_menu(root):
     menu = tk.Frame(root)
     menu.pack(fill='both', expand=True)
 
-
-    aboutBtn = tkinter.Button(menu, text = "About")  # command would be to open About page
+    aboutBtn = tk.Button(menu, text = "About")  # command would be to open About page
     aboutBtn.pack(padx=10, pady=10)
 
-    setclockBtn = tkinter.Button(menu, text = "Set Clock")  # command would be to open SetClock page
+    setclockBtn = tk.Button(menu, text = "Set Clock")  # command would be to open SetClock page
     setclockBtn.pack(padx=10, pady=10)
 
-    newpatientBtn = tkinter.Button(menu, text = "New Patient")  # command would be to open New Patient
+    newpatientBtn = tk.Button(menu, text = "New Patient")  # command would be to open New Patient
     newpatientBtn.pack(padx=10, pady=10)
 
-    modeBtn = tkinter.Button(menu, text = "Modes")  # command would be to open the Mode page
+    modeBtn = tk.Button(menu, text = "Modes")  # command would be to open the Mode page
     modeBtn.pack(padx=10, pady=10)
 
-    aboutBtn = tk.Button(menu, text="About")  # command would be to open the next window
-    aboutBtn.pack(padx=10, pady=10)
-
-    setclockBtn = tk.Button(menu, text="Set Clock")
-    setclockBtn.pack(padx=10, pady=10)
-
-    newpatientBtn = tk.Button(menu, text="New Patient")
-    newpatientBtn.pack(padx=10, pady=10)
-
-    modeBtn = tk.Button(menu, text="Mode")
-    modeBtn.pack(padx=10, pady=10)
 
     menu.mainloop()
 
